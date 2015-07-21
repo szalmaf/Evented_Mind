838dbac8053f0301461baf0ba4c22ff263f7d958
test <test@test.com>
2015-07-21 12:58:57 -0400

    Compact object literal way of creating object

--- JavaScript Objects/client/app.js
+++ JavaScript Objects/client/app.js
@@ -5,16 +5,26 @@
 // person = new Object();
 
 // Create object using the create method
-person = Object.create();
+// person = Object.create();
 
 // Add 'name' property to object with the value "Chris"
 // Properties are name-value pairs
 // person.name = "Chris;"
 // person.year = 2014;
-person.getName = function(){ return this.name;};
+// person.getName = function(){ return this.name;};
 
 // Add properties using index notation. Useful when a property's name
 // is in a variable
-person['year'] = 2014;
-var prop = 'name';
-person[prop] = 'Chris'
+// person['year'] = 2014;
+// var prop = 'name';
+// person[prop] = 'Chris'
+
+// Compact object literal way of creating object with 
+// properties, methods using key-value pairs
+person = {
+	name: 'Chris';
+	year: 2014;
+	getName: function(){
+		return name;
+	}
+};
