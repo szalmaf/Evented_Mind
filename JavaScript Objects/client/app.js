47adc08a61e7a1e3aacb50a6ff69bbff16c9913b
test <test@test.com>
2015-07-21 12:49:08 -0400

    Add properties using index notation

--- JavaScript Objects/client/app.js
+++ JavaScript Objects/client/app.js
@@ -3,6 +3,12 @@ person = {};
 
 // Add 'name' property to object with the value "Chris"
 // Properties are name-value pairs
-person.name = "Chris;"
-person.year = 2014;
+// person.name = "Chris;"
+// person.year = 2014;
 person.getName = function(){ return this.name;};
+
+// Add properties using index notation. Useful when a property's name
+// is in a variable
+person['year'] = 2014;
+var prop = 'name';
+person[prop] = 'Chris'
